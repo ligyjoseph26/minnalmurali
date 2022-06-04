635f9ccda2a0c23705c7c32465d03cc5e885d3a5
Anonymous <102202494+ligyjoseph26@users.noreply.github.com>
2022-06-04 13:04:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,7 +97,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action("Typing")
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAIeM2KbCkETnqM14jq_jxuXLiO_se7EAAIVAQACyJRkFGZEMKKnFWwTHgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
